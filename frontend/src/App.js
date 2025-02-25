0d7458dd30ec003081499ab1ec23f8164b5417ec
daniil <danildryzov@gmail.com>
2025-02-25 14:47:10 +0300

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import GrammarAnalysis from './components/grammar/GrammarAnalysis';
 
 function App() {
