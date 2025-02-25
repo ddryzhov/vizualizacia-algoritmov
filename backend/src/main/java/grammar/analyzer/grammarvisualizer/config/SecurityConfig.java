e487bc870cc09fa4ccd7814208f382f6450596c7
daniil <danildryzov@gmail.com>
2025-02-25 16:39:53 +0300

    Removed checkstyle for Railway deploymentfwdfwdfdewfcd

--- backend/src/main/java/grammar/analyzer/grammarvisualizer/config/SecurityConfig.java
+++ backend/src/main/java/grammar/analyzer/grammarvisualizer/config/SecurityConfig.java
@@ -15,7 +15,13 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://ddryzhov.github.io",
+                "https://vizualizacia-algoritmov-production.up.railway.app"
+        ));
+
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
